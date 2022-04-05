fb7b7b801283478d5c66ddc26f0da645503ffaec
Quentin <q.danna@laposte.net>
2022-04-05 12:46:48 +0200

    Initialisation du projet

---
+++ templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Literie3000</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="container">
+        <div class="logo-wrapper">
+            <a href="index.php">
+                <img src="img/logo_transparent.png" alt="Logo Literie 3000" class="logo">
+            </a>
+        </div>
